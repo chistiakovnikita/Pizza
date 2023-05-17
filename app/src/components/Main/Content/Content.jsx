d47bb15d3ff8b464069c4acfb4b634b6c109d5b1
chistiakovnikita <nikita_chnv@mail.ru>
2023-05-17 04:24:26 +0300

    createAsyncThunk, refactoring

--- app/src/components/Main/Content/Content.jsx
+++ app/src/components/Main/Content/Content.jsx
@@ -1,13 +1,12 @@
-import { useContext, useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import qs from 'qs'
+import { useCallback, useContext, useEffect} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../../../App'
+import qs from 'qs'
 
-import axios from 'axios'
-
+import { fetchPizzas } from '../../../redux/slices/pizzasSlice'
 import PizzaCard from '../PizzaCard'
 import Skeleton from '../Skeleton'
-import { AppContext } from '../../../App'
 
 import './content.scss'
 
@@ -16,21 +15,26 @@ const Content = () => {
     const { categoryIndex, sortIndex } = useSelector(
         (state) => state.filterSlice
     )
-    const [data, setData] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
+    const { data, status } = useSelector((state) => state.pizzasSlice)
+
+    const dispatch = useDispatch()
 
     const navigate = useNavigate()
     const category = categoryIndex > 0 ? `category=${categoryIndex}` : ''
-    
+
+    const getData = useCallback(async () => {
+        dispatch(
+            fetchPizzas({
+                category, //параметры category,sortIndex,searchValue необходимо передать в  AsyncThunk
+                sortIndex,
+                searchValue,
+            })
+        )
+    }, [category, searchValue, sortIndex, dispatch])
+
     useEffect(() => {
-        setIsLoading(true)
-        axios
-            .get(
-                `https://6457bacd0c15cb14820f9c6c.mockapi.io/pizzas?${category}&sortBy=${sortIndex.sort}&search=${searchValue}`
-            )
-            .then((response) => setData(response.data))
-            .finally(() => setIsLoading(false))
-    }, [category, sortIndex, searchValue])
+        getData()
+    }, [category, sortIndex, searchValue, getData])
 
     useEffect(() => {
         const queryString = qs.stringify({
@@ -45,15 +49,20 @@ const Content = () => {
         <section className="content">
             <div className="container">
                 <h2 className="content__title">Все пиццы</h2>
-                <div className="content__inner">
-                    {isLoading
-                        ? [...new Array(4)].map((_, index) => (
-                              <Skeleton key={index} />
-                          ))
-                        : data.map((pizza) => (
-                              <PizzaCard key={pizza.id} {...pizza} />
-                          ))}
-                </div>
+
+                {status === 'error' ? (
+                    <div>ошибка</div>
+                ) : (
+                    <div className="content__inner">
+                        {status === 'loading'
+                            ? [...new Array(4)].map((_, index) => (
+                                  <Skeleton key={index} />
+                              ))
+                            : data.map((pizza) => (
+                                  <PizzaCard key={pizza.id} {...pizza} />
+                              ))}
+                    </div>
+                )}
             </div>
         </section>
     )
--- app/src/components/pages/CartPage/CartPage.jsx
+++ app/src/components/pages/CartPage/CartPage.jsx
@@ -3,16 +3,9 @@ import Cart from '../../Main/Cart'
 import CartEmpty from '../../Main/CartEmpty'
 
 const CartPage = () => {
+    const products = useSelector((state) => state.cartSlice.products)
 
-    const products = useSelector((state) =>state.cartSlice.products)
-
-    return (
-        <>
-        {products.length > 0 ? <Cart />:<CartEmpty />}
-           
-            
-        </>
-    )
+    return <>{products.length > 0 ? <Cart /> : <CartEmpty />}</>
 }
 
 export default CartPage
